99041e88bc709709e09847353a7bdd0d75993f90
oetom <oediting@yahoo.de>
2024-08-18 18:42:10 +0200

    new line from integration 1 to test pulling

--- hello_git.py
+++ hello_git.py
@@ -1,3 +1,5 @@
 print("Hello Git!")
 print("Das ist eine Aenderung direkt aus GitHub")
 print("Diese Anpassung ist aus Integration 1")
+
+print("Hier unten was neues aus Int1 - mal schauen ob er pulled")
